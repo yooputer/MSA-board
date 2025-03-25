d2f693eb10a07d659c19e71a6775bb937bbb0ed5
김유진 <yoojin027@mteletec.com>
2025-03-25 09:22:47 +0900

    initial commit

---
+++ src/main/java/org/yooputer/msaboard/MsaBoardApplication.java
@@ -0,0 +1,13 @@
+package org.yooputer.msaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaBoardApplication.class, args);
+    }
+
+}
