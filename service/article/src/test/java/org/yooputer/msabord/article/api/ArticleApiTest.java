ab9810290989b017412a91326be11a351476f515
김유진 <yoojin027@mteletec.com>
2025-03-27 13:37:47 +0900

    ✅test: Article CRUD 테스트, 대용량 테스트 데이터 생성 코드 추가

---
+++ service/article/src/test/java/org/yooputer/msabord/article/api/ArticleApiTest.java
@@ -0,0 +1,139 @@
+package org.yooputer.msabord.article.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+import org.yooputer.msabord.article.service.response.ArticleResponse;
+
+
+public class ArticleApiTest {
+    RestClient restClient = RestClient.create("http://localhost:9000");
+
+    @Test
+    void createTest() {
+        ArticleResponse response = create(new ArticleCreateRequest(
+                "hi", "my content", 1L, 1L
+        ));
+        System.out.println("response = " + response);
+    }
+
+    ArticleResponse create(ArticleCreateRequest request) {
+        return restClient.post()
+                .uri("/v1/articles")
+                .body(request)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    @Test
+    void readTest() {
+        ArticleResponse response = read(121530268440289280L);
+        System.out.println("response = " + response);
+    }
+
+    ArticleResponse read(Long articleId) {
+        return restClient.get()
+                .uri("/v1/articles/{articleId}", articleId)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    @Test
+    void updateTest() {
+        update(121530268440289280L);
+        ArticleResponse response = read(121530268440289280L);
+        System.out.println("response = " + response);
+    }
+
+    void update(Long articleId) {
+        restClient.put()
+                .uri("/v1/articles/{articleId}", articleId)
+                .body(new ArticleUpdateRequest("hi 2", "my content 22"))
+                .retrieve();
+    }
+
+    @Test
+    void deleteTest() {
+        restClient.delete()
+                .uri("/v1/articles/{articleId}", 121530268440289280L)
+                .retrieve();
+    }
+//
+//    @Test
+//    void readAllTest() {
+//        ArticlePageResponse response = restClient.get()
+//                .uri("/v1/articles?boardId=1&pageSize=30&page=50000")
+//                .retrieve()
+//                .body(ArticlePageResponse.class);
+//
+//        System.out.println("response.getArticleCount() = " + response.getArticleCount());
+//        for (ArticleResponse article : response.getArticles()) {
+//            System.out.println("articleId = " + article.getArticleId());
+//        }
+//    }
+//
+//    @Test
+//    void readAllInfiniteScrollTest() {
+//        List<ArticleResponse> articles1 = restClient.get()
+//                .uri("/v1/articles/infinite-scroll?boardId=1&pageSize=5")
+//                .retrieve()
+//                .body(new ParameterizedTypeReference<List<ArticleResponse>>() {
+//                });
+//
+//        System.out.println("firstPage");
+//        for (ArticleResponse articleResponse : articles1) {
+//            System.out.println("articleResponse.getArticleId() = " + articleResponse.getArticleId());
+//        }
+//
+//        Long lastArticleId = articles1.getLast().getArticleId();
+//        List<ArticleResponse> articles2 = restClient.get()
+//                .uri("/v1/articles/infinite-scroll?boardId=1&pageSize=5&lastArticleId=%s".formatted(lastArticleId))
+//                .retrieve()
+//                .body(new ParameterizedTypeReference<List<ArticleResponse>>() {
+//                });
+//
+//        System.out.println("secondPage");
+//        for (ArticleResponse articleResponse : articles2) {
+//            System.out.println("articleResponse.getArticleId() = " + articleResponse.getArticleId());
+//        }
+//    }
+//
+//    @Test
+//    void countTest() {
+//        ArticleResponse response = create(new ArticleCreateRequest("hi", "content", 1L, 2L));
+//
+//        Long count1 = restClient.get()
+//                .uri("/v1/articles/boards/{boardId}/count", 2L)
+//                .retrieve()
+//                .body(Long.class);
+//        System.out.println("count1 = " + count1); // 1
+//
+//        restClient.delete()
+//                .uri("/v1/articles/{articleId}", response.getArticleId())
+//                .retrieve();
+//
+//        Long count2 = restClient.get()
+//                .uri("/v1/articles/boards/{boardId}/count", 2L)
+//                .retrieve()
+//                .body(Long.class);
+//        System.out.println("count2 = " + count2); // 0
+//    }
+//
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleCreateRequest {
+        private String title;
+        private String content;
+        private Long writerId;
+        private Long boardId;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleUpdateRequest {
+        private String title;
+        private String content;
+    }
+}
