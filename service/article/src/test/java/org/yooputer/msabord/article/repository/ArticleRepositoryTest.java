4ff5d30c82c462ba321b1a3f931ffdab6f5ead68
김유진 <yoojin027@mteletec.com>
2025-03-28 10:05:44 +0900

    ✅test: [Article] ArticleRepository 메서드 테스트 코드 추가

---
+++ service/article/src/test/java/org/yooputer/msabord/article/repository/ArticleRepositoryTest.java
@@ -0,0 +1,32 @@
+package org.yooputer.msabord.article.repository;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.yooputer.msabord.article.entity.Article;
+
+import java.util.List;
+
+@Slf4j
+@SpringBootTest
+class ArticleRepositoryTest {
+
+    @Autowired
+    ArticleRepository articleRepository;
+
+    @Test
+    void findAllTest() {
+        List<Article> articles = articleRepository.findAll(1L, 9970L, 30L);
+        log.info("articles.size = {}", articles.size());
+        for (Article article : articles) {
+            log.info("article = {}", article);
+        }
+    }
+
+    @Test
+    void countTest() {
+        Long count = articleRepository.count(1L, 10000L);
+        log.info("count = {}", count);
+    }
+}
