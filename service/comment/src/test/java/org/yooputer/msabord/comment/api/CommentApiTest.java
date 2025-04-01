53a6eb70bd67e747c6718b67eb623bf8d4e7a062
김유진 <yoojin027@mteletec.com>
2025-04-01 09:32:57 +0900

    ✅test: [Comment] 댓글 생성, 삭제 테스트코드 추가

---
+++ service/comment/src/test/java/org/yooputer/msabord/comment/api/CommentApiTest.java
@@ -0,0 +1,57 @@
+package org.yooputer.msabord.comment.api;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+import org.yooputer.msabord.comment.service.response.CommentResponse;
+
+public class CommentApiTest {
+    RestClient restClient = RestClient.create("http://localhost:9001");
+
+    @Test
+    void create() {
+        CommentResponse response1 = createComment(new CommentCreateRequest(1L, "my comment1", null, 1L));
+        CommentResponse response2 = createComment(new CommentCreateRequest(1L, "my comment2", response1.getCommentId(), 1L));
+        CommentResponse response3 = createComment(new CommentCreateRequest(1L, "my comment3", response1.getCommentId(), 1L));
+
+        System.out.println("commentId=%s".formatted(response1.getCommentId()));
+        System.out.println("\tcommentId=%s".formatted(response2.getCommentId()));
+        System.out.println("\tcommentId=%s".formatted(response3.getCommentId()));
+    }
+
+    CommentResponse createComment(CommentCreateRequest request) {
+        return restClient.post()
+                .uri("/v1/comments")
+                .body(request)
+                .retrieve()
+                .body(CommentResponse.class);
+    }
+
+    @Test
+    void read() {
+        CommentResponse response = restClient.get()
+                .uri("/v1/comments/{commentId}", 165254851549220864L)
+                .retrieve()
+                .body(CommentResponse.class);
+
+        System.out.println("response = " + response);
+    }
+
+    @Test
+    void delete() {
+        restClient.delete()
+                .uri("/v1/comments/{commentId}", 165254851549220864L)
+                .retrieve();
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class CommentCreateRequest {
+        private Long articleId;
+        private String content;
+        private Long parentCommentId;
+        private Long writerId;
+    }
+}
