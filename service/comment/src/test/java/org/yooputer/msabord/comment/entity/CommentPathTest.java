b47574c34682ea65df57df9e3db1caa881d321d6
김유진 <yoojin027@mteletec.com>
2025-04-02 08:49:52 +0900

    ✅test: [Comment] CommentPath 테스트 코드 추가

---
+++ service/comment/src/test/java/org/yooputer/msabord/comment/entity/CommentPathTest.java
@@ -0,0 +1,51 @@
+package org.yooputer.msabord.comment.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class CommentPathTest {
+    @Test
+    void createChildCommentTest() {
+        // 00000 <- 생성
+        createChildCommentTest(CommentPath.create(""), null, "00000");
+
+        // 00000
+        //      00000 <- 생성
+        createChildCommentTest(CommentPath.create("00000"), null, "0000000000");
+
+        // 00000
+        // 00001 <- 생성
+        createChildCommentTest(CommentPath.create(""), "00000", "00001");
+
+        // 0000z
+        //      abcdz
+        //          zzzzz
+        //              zzzzz
+        //      abce0 <- 생성
+        createChildCommentTest(CommentPath.create("0000z"), "0000zabcdzzzzzzzzzzz", "0000zabce0");
+    }
+
+    void createChildCommentTest(CommentPath commentPath, String descendantsTopPath, String expectedChildPath) {
+        CommentPath childCommentPath = commentPath.createChildCommentPath(descendantsTopPath);
+        assertThat(childCommentPath.getPath()).isEqualTo(expectedChildPath);
+    }
+
+    @Test
+    void createChildCommentPathIfMaxDepthTest() {
+        assertThatThrownBy(() ->
+                CommentPath.create("zzzzz".repeat(5)).createChildCommentPath(null)
+        ).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void createChildCommentPathIfChunkOverflowTest() {
+        // given
+        CommentPath commentPath = CommentPath.create("");
+
+        // when, then
+        assertThatThrownBy(() -> commentPath.createChildCommentPath("zzzzz"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
