0e9ce85f57fb043f70ef77bedf9d0a92ff63631e
김유진 <yoojin027@mteletec.com>
2025-04-03 08:51:01 +0900

    📝docs: [SQL] like 관련 DDL문 추가

---
+++ service/like/src/main/resources/db/rdb-schema.sql
@@ -0,0 +1,24 @@
+create table article_like (
+    article_like_id bigint not null primary key,
+    article_id bigint not null,
+    user_id bigint not null,
+    created_at datetime not null
+);
+
+create unique index idx_article_id_user_id on article_like(article_id asc, user_id asc);
+
+create table article_like_count (
+    article_id bigint not null primary key,
+    like_count bigint not null,
+    version bigint not null
+);
+
+create table outbox (
+    outbox_id bigint not null primary key,
+    shard_key bigint not null,
+    event_type varchar(100) not null,
+    payload varchar(5000) not null,
+    created_at datetime not null
+);
+
+create index idx_shard_key_created_at on outbox(shard_key asc, created_at asc);
