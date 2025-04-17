50fc376c93b94a091f3fed3fed9ccda5415936fd
김유진 <yoojin027@mteletec.com>
2025-04-17 10:32:27 +0900

    ✨feat: [Like] 인기게시글 관련 OutboxEventRelay 적용

--- service/like/src/main/java/org/yooputer/msabord/like/LikeApplication.java
+++ service/like/src/main/java/org/yooputer/msabord/like/LikeApplication.java
@@ -2,8 +2,12 @@ package org.yooputer.msabord.like;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan(basePackages = "org.yooputer.msabord")
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "org.yooputer.msabord")
 public class LikeApplication {
     public static void main(String[] args) {
         SpringApplication.run(LikeApplication.class, args);
--- service/like/src/main/java/org/yooputer/msabord/like/service/ArticleLikeService.java
+++ service/like/src/main/java/org/yooputer/msabord/like/service/ArticleLikeService.java
@@ -3,6 +3,10 @@ package org.yooputer.msabord.like.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.yooputer.msabord.common.event.EventType;
+import org.yooputer.msabord.common.event.payload.ArticleLikedEventPayload;
+import org.yooputer.msabord.common.event.payload.ArticleUnlikedEventPayload;
+import org.yooputer.msabord.common.outboxmessagerelay.OutboxEventPublisher;
 import org.yooputer.msabord.common.snowflake.Snowflake;
 import org.yooputer.msabord.like.entity.ArticleLike;
 import org.yooputer.msabord.like.entity.ArticleLikeCount;
@@ -16,6 +20,7 @@ public class ArticleLikeService {
     private final Snowflake snowflake = new Snowflake();
     private final ArticleLikeRepository articleLikeRepository;
     private final ArticleLikeCountRepository articleLikeCountRepository;
+    private final OutboxEventPublisher outboxEventPublisher;
 
     public ArticleLikeResponse read(Long articleId, Long userId) {
         return articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
@@ -43,6 +48,18 @@ public class ArticleLikeService {
                     ArticleLikeCount.init(articleId, 1L)
             );
         }
+
+        outboxEventPublisher.publish(
+                EventType.ARTICLE_LIKED,
+                ArticleLikedEventPayload.builder()
+                        .articleLikeId(articleLike.getArticleLikeId())
+                        .articleId(articleLike.getArticleId())
+                        .userId(articleLike.getUserId())
+                        .createdAt(articleLike.getCreatedAt())
+                        .articleLikeCount(count(articleLike.getArticleId()))
+                        .build(),
+                articleLike.getArticleId()
+        );
     }
 
     @Transactional
@@ -51,6 +68,18 @@ public class ArticleLikeService {
                 .ifPresent(articleLike -> {
                     articleLikeRepository.delete(articleLike);
                     articleLikeCountRepository.decrease(articleId);
+
+                    outboxEventPublisher.publish(
+                            EventType.ARTICLE_UNLIKED,
+                            ArticleUnlikedEventPayload.builder()
+                                    .articleLikeId(articleLike.getArticleLikeId())
+                                    .articleId(articleLike.getArticleId())
+                                    .userId(articleLike.getUserId())
+                                    .createdAt(articleLike.getCreatedAt())
+                                    .articleLikeCount(count(articleLike.getArticleId()))
+                                    .build(),
+                            articleLike.getArticleId()
+                    );
                 });
     }
 
