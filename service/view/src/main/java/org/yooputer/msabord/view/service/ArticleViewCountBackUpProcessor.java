77696da9839df1902ed0754b4c492aee7f2ba021
김유진 <yoojin027@mteletec.com>
2025-04-17 10:32:49 +0900

    ✨feat: [Article-View] 인기게시글 관련 OutboxEventRelay 적용

--- service/view/src/main/java/org/yooputer/msabord/view/service/ArticleViewCountBackUpProcessor.java
+++ service/view/src/main/java/org/yooputer/msabord/view/service/ArticleViewCountBackUpProcessor.java
@@ -4,6 +4,9 @@ package org.yooputer.msabord.view.service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.yooputer.msabord.common.event.EventType;
+import org.yooputer.msabord.common.event.payload.ArticleViewedEventPayload;
+import org.yooputer.msabord.common.outboxmessagerelay.OutboxEventPublisher;
 import org.yooputer.msabord.view.entity.ArticleViewCount;
 import org.yooputer.msabord.view.repository.ArticleViewCountBackUpRepository;
 
@@ -11,6 +14,7 @@ import org.yooputer.msabord.view.repository.ArticleViewCountBackUpRepository;
 @RequiredArgsConstructor
 public class ArticleViewCountBackUpProcessor {
     private final ArticleViewCountBackUpRepository articleViewCountBackUpRepository;
+    private final OutboxEventPublisher outboxEventPublisher;
 
     @Transactional
     public void backUp(Long articleId, Long viewCount) {
@@ -21,5 +25,14 @@ public class ArticleViewCountBackUpProcessor {
                             () -> articleViewCountBackUpRepository.save(ArticleViewCount.init(articleId, viewCount))
                     );
         }
+
+        outboxEventPublisher.publish(
+                EventType.ARTICLE_VIEWED,
+                ArticleViewedEventPayload.builder()
+                        .articleId(articleId)
+                        .articleViewCount(viewCount)
+                        .build(),
+                articleId
+        );
     }
 }
