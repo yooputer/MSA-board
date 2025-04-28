88984059d0fb340c47562c791b29566e648492b2
김유진 <yoojin027@mteletec.com>
2025-04-28 16:27:12 +0900

    ✅test: [Article-Read] 게시글 조회수 Client 테스트 코드 추가

---
+++ service/article-read/src/test/java/org/yooputer/msabord/article_read/client/ViewClientTest.java
@@ -0,0 +1,46 @@
+package org.yooputer.msabord.article_read.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
+class ViewClientTest {
+    @Autowired
+    ViewClient viewClient;
+
+    @Test
+    void readCacheableTest() throws InterruptedException {
+        viewClient.count(1L); // 로그 출력
+        viewClient.count(1L); // 로그 미출력
+        viewClient.count(1L); // 로그 미출력
+
+        TimeUnit.SECONDS.sleep(3);
+        viewClient.count(1L); // 로그 출력
+    }
+
+    @Test
+    void readCacheableMultiThreadTest() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        viewClient.count(1L); // init cache
+
+        for(int i=0;i <5; i++) {
+            CountDownLatch latch = new CountDownLatch(5);
+            for(int j=0;j<5;j++) {
+                executorService.submit(() -> {
+                    viewClient.count(1L);
+                    latch.countDown();
+                });
+            }
+            latch.await();
+            TimeUnit.SECONDS.sleep(2);
+            System.out.println("=== cache expired ===");
+        }
+    }
+}
